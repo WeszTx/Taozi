7d9a1fe77b0fe087c68b0eb73a06a20ccf3e5832
Taozi <tao_helloworld@foxmail.com>
2020-09-25 16:53:03 +0800

    添加工程

---
+++ git_test/src/com/litao/git/GitTest.java
@@ -0,0 +1,20 @@
+package com.litao.git;
+
+/**
+ * @Description
+ * @Author Taozi
+ * @Date 2020/9/25 16:37
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("----------------添加记录1--------------");
+        System.out.println("----------------添加记录2--------------");
+        System.out.println("----------------添加记录3--------------");
+        System.out.println("----------------添加记录4--------------");
+        System.out.println("----------------添加记录5--------------");
+        System.out.println("----------------添加记录6--------------");
+        System.out.println("----------------添加记录7--------------");
+        System.out.println("----------------添加记录8--------------");
+    }
+}
